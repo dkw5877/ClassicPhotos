31c1f6081c35b842273b3988c013b843e6902c0b
dkw5877 <dkw5877@gmail.com>
2014-07-14 19:55:53 -0700

    implement initializer and main methods for ImageDownloader class

--- ClassicPhotos/ImageDownloader.h
+++ ClassicPhotos/ImageDownloader.h
@@ -7,7 +7,26 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "PhotoRecord.h" 
+
+@protocol ImageDownloaderDelegate;
 
 @interface ImageDownloader : NSOperation
 
+@property(nonatomic, assign)id <ImageDownloaderDelegate> delegate;
+
+@property(nonatomic, readonly)NSIndexPath* indexPath;
+@property(nonatomic, readonly)PhotoRecord* photoRecord;
+
+-(id)initWithPhotoRecord:(PhotoRecord*)photoRecord atIndexPath:(NSIndexPath*)indexPath delegate:(id<ImageDownloaderDelegate>)theDelegate;
+
 @end
+
+
+//declare the delegate protocol
+@protocol ImageDownloaderDelegate <NSObject>
+
+-(void)imageDownloaderDidFinish:(ImageDownloader*)downloader;
+
+@end
+
--- ClassicPhotos/ImageDownloader.m
+++ ClassicPhotos/ImageDownloader.m
@@ -10,4 +10,58 @@
 
 @implementation ImageDownloader
 
+- (id)initWithPhotoRecord:(PhotoRecord *)photoRecord atIndexPath:(NSIndexPath *)indexPath delegate:(id<ImageDownloaderDelegate>)theDelegate
+{
+    self = [super init];
+    
+    if (self) {
+        _photoRecord = photoRecord;
+        _indexPath =  indexPath;
+        _delegate = theDelegate;
+    }
+    return self;
+
+}
+
+//override the main method
+
+- (void)main
+{
+    //create an autorelease pool
+    @autoreleasepool {
+        
+        //if the operation was canceled before we begin download just return
+        if (self.isCancelled) {
+            return;
+        }
+        
+        NSData* imageData = [[NSData alloc]initWithContentsOfURL:self.photoRecord.url];
+        
+        //if the operation is canceled after we begin download, then nil the property and return
+        if (self.isCancelled) {
+            imageData = nil;
+            return;
+        }
+        
+        //if we retrieved the image, set it in the photo record, otherwise indicate failure
+        if (imageData) {
+            self.photoRecord.image = [UIImage imageWithData:imageData];
+        }
+        else{
+            self.photoRecord.failed = YES;
+        }
+        
+        imageData = nil; //clear the variable since we are done
+        
+        //once again, check if the operation was canceled, if so just return we are already done
+        if (self.isCancelled) {
+            return;
+        }
+        
+        //finally, call the delegate method back on the main thread. We also need to cast it to an NSObject to get the performSelectorOnMainThread method
+        [(NSObject*)self.delegate performSelectorOnMainThread:@selector(imageDownloaderDidFinish:) withObject:self waitUntilDone:NO];
+        
+    }
+}
+
 @end

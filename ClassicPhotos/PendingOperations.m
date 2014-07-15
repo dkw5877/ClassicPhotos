5742c6ab513cf03691a164d3e41e9f3769f32976
dkw5877 <dkw5877@gmail.com>
2014-07-14 19:21:33 -0700

    implement custom getters for PhotoRecord class

---
+++ ClassicPhotos/PendingOperations.m
@@ -0,0 +1,13 @@
+//
+//  PendingOperations.m
+//  ClassicPhotos
+//
+//  Created by user on 7/14/14.
+//  Copyright (c) 2014 someCompanyNameHere. All rights reserved.
+//
+
+#import "PendingOperations.h"
+
+@implementation PendingOperations
+
+@end
--- ClassicPhotos/PhotoRecord.m
+++ ClassicPhotos/PhotoRecord.m
@@ -10,4 +10,19 @@
 
 @implementation PhotoRecord
 
+-(BOOL)hasImage
+{
+    return _image != nil;
+}
+
+-(BOOL)isFiltered
+{
+    return _filtered;
+}
+
+-(BOOL)isFailed
+{
+    return _failed;
+}
+
 @end

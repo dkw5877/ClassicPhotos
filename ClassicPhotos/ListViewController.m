33cbc9d2bf832a3965db65fc6b16106d2c29cedb
dkw5877 <dkw5877@gmail.com>
2014-07-14 17:13:13 -0700

    implement table view controller data source and related methods

--- ClassicPhotos/ListViewController.m
+++ ClassicPhotos/ListViewController.m
@@ -24,6 +24,22 @@
     return self;
 }
 
+//use lazy instantiation
+-(NSDictionary*)photos
+{
+    if(!_photos)
+    {
+        //create the URL
+        NSURL* dataSourceURL = [NSURL URLWithString:kDatasourceURLString];
+        
+        //load the photos data soure
+        _photos = [[NSDictionary alloc]initWithContentsOfURL:dataSourceURL];
+    }
+    
+    return _photos;
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -33,7 +49,12 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    self.title = @"Classic Photos";
+    self.tableView.rowHeight = 80;
+    
 }
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -42,79 +63,65 @@
 }
 
 #pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return self.photos.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    static NSString* kCellIdentifier = @"Cell Indentifier";
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:kCellIdentifier];
     
-    // Configure the cell...
+    if (!cell)
+    {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kCellIdentifier];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
     
+    NSString* rowKey = [[self.photos allKeys]objectAtIndex:indexPath.row];
+    NSURL* url = [NSURL URLWithString:[self.photos valueForKey:rowKey]];
+    NSData* imageData = [NSData dataWithContentsOfURL:url];
+    UIImage* image = nil;
+    
+    if (imageData)
+    {
+        UIImage* unfilteredImage = [UIImage imageWithData:imageData];
+        image = [self applySepiaFilterToImage: unfilteredImage];
+    }
+    
+    cell.textLabel.text = rowKey;
+    cell.imageView.image = image;
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+#pragma mark - Image Filtering Methods
+-(UIImage*)applySepiaFilterToImage:(UIImage*)image
 {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+    //convert the UIImage to a CIImage
+    CIImage* inputImage = [CIImage imageWithData:UIImagePNGRepresentation(image)];
+    UIImage* sepiaImage = nil;
+    
+    //create the Core Image context
+    CIContext* context = [CIContext contextWithOptions:nil];
+    
+    //create the CIFilter
+    CIFilter* filter = [CIFilter filterWithName:@"CISepiaTone" keysAndValues:kCIInputImageKey,inputImage,@"inputIntensity",[NSNumber numberWithFloat:0.8], nil];
+    
+    //get the output image from the filter
+    CIImage* outputImage = [filter outputImage];
+    
+    //create a core graphics image reference
+    CGImageRef outputImageRef = [context createCGImage:outputImage fromRect:[outputImage extent]];
+    
+    //create the UIImage
+    sepiaImage = [UIImage imageWithCGImage:outputImageRef];
+    
+    CGImageRelease(outputImageRef);
+    
+    return sepiaImage;
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end

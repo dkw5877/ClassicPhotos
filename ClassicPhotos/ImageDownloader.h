be65e5f31f45629bf55f520e5400b97470ff5686
dkw5877 <dkw5877@gmail.com>
2014-07-14 19:35:56 -0700

    create ImageDownloader class

---
+++ ClassicPhotos/ImageDownloader.h
@@ -0,0 +1,13 @@
+//
+//  ImageDownloader.h
+//  ClassicPhotos
+//
+//  Created by user on 7/14/14.
+//  Copyright (c) 2014 someCompanyNameHere. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageDownloader : NSOperation
+
+@end
---
+++ ClassicPhotos/ImageDownloader.m
@@ -0,0 +1,13 @@
+//
+//  ImageDownloader.m
+//  ClassicPhotos
+//
+//  Created by user on 7/14/14.
+//  Copyright (c) 2014 someCompanyNameHere. All rights reserved.
+//
+
+#import "ImageDownloader.h"
+
+@implementation ImageDownloader
+
+@end

8ebe5be0052ffa75f016009a91cc2c507c7e41b0
dkw5877 <dkw5877@gmail.com>
2014-07-14 19:33:12 -0700

    implement getter methods for properties of PendingOperations class

--- ClassicPhotos/PendingOperations.m
+++ ClassicPhotos/PendingOperations.m
@@ -10,4 +10,48 @@
 
 @implementation PendingOperations
 
+-(NSMutableDictionary*)downloadsInProgress
+{
+    if (!_downloadsInProgress) {
+        _downloadsInProgress = [[NSMutableDictionary alloc]init];
+    }
+    
+    return _downloadsInProgress;
+}
+
+
+-(NSOperationQueue*)downloadQueue
+{
+    if (!_downloadQueue) {
+        _downloadQueue = [[NSOperationQueue alloc]init];
+        _downloadQueue.name = @"Download Queue";
+        _downloadQueue.maxConcurrentOperationCount = 1;
+    }
+    
+    return _downloadQueue;
+}
+
+
+
+-(NSMutableDictionary*)filtrationInProgress
+{
+    if (!_filtrationInProgress) {
+        _filtrationInProgress = [[NSMutableDictionary alloc]init];
+    }
+    
+    return _filtrationInProgress;
+}
+
+-(NSOperationQueue*)filtrationQueue
+{
+    if (!_filtrationQueue) {
+        _filtrationQueue = [[NSOperationQueue alloc]init];
+        _filtrationQueue.name = @"Image Filtration Queue";
+        _filtrationQueue.maxConcurrentOperationCount = 1;
+        
+    }
+    
+    return _filtrationQueue;
+}
+
 @end
